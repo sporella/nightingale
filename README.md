d39ac150dc4d11bcdc36cb17e27507a6cb123fd1
sporella <sporella@uc.cl>
2020-05-13 15:06:00 -0400

    dos

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Códigos visualizaciones desafío #30díasdegráficos
 - Día 1: barras/columnas
 
 [Análisis datos Spotify](https://sporella.netlify.app/2020-05-12-m%C3%BAsica-datos-y-gr%C3%A1ficos/)
+
+
+- Día 2: líneas
+
+[Visualización con highcharter]()
---
+++ dos.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(highcharter)
+
+data <- read_csv("data/genero.csv")
+
+data_pivot <- data %>% 
+  pivot_longer(cols = starts_with("BRECHA"), 
+               names_to = c("year", "tipo"), 
+               names_pattern = "BRECHA_(.*)_(.*)")
+
+
+hchart(data_pivot %>%  filter(tipo =="PROFESIONAL"), "line",
+       hcaes(x = year, y = value, group = NOM_REGION)) %>% 
+       hc_title(text="Brecha de género entre personas tituladas en carreras del área de tecnología") %>% 
+  hc_subtitle(text="Carreras profesionales") %>% 
+  hc_credits(enabled = TRUE, text = "@sporella", href = "https://sporella.netlify.app/") %>% 
+  hc_yAxis(title= list(text = "Brecha")) %>% 
+  hc_xAxis(title= list(text = "")) %>% 
+  hc_add_theme(hc_theme_darkunica(credits = list(
+    style = list(
+      size = 3,
+      color = "white"
+    ))))
+
+hchart(data_pivot %>%  filter(tipo =="TECNICO"), "line",
+       hcaes(x = year, y = value, group = NOM_REGION)) %>% 
+  hc_title(text="Brecha de género entre personas tituladas en carreras del área de tecnología") %>% 
+  hc_subtitle(text="Carreras técnicas") %>% 
+  hc_credits(enabled = TRUE, text = "@sporella", href = "https://sporella.netlify.app/") %>% 
+  hc_yAxis(title= list(text = "Brecha")) %>% 
+  hc_xAxis(title= list(text = "")) %>% 
+  hc_add_theme(hc_theme_darkunica(credits = list(
+    style = list(
+      size = 3,
+      color = "white"
+    ))))
